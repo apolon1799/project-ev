313b127dd03697da3ebd9b984a1f4b991301a6ca
apolon1799 <apolonicloud@gmail.com>
2025-09-20 14:02:28 +0400

    Fix node.js build issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/project-ev/',
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+  },
+  define: {
+    global: 'globalThis',
+  },
 })
